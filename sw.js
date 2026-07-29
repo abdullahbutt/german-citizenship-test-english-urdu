3cfa8dbb876c0b841b4bf9d405d8d4f25a822c24
abdullahbutt <abdullah.butt@msn.com>
2026-07-29 02:19:38 +0200

    udpated og-image

--- sw.js
+++ sw.js
@@ -13,7 +13,7 @@
 
 // Cache key includes a build timestamp so every deploy invalidates the cache.
 // The build timestamp is replaced at build time by build.js.
-const CACHE_VERSION = '2026-07-11T12-27-44-692Z';
+const CACHE_VERSION = '2026-07-29T00-18-36-547Z';
 const CACHE_NAME = `gct-cache-${CACHE_VERSION}`;
 
 // All same-origin pages to pre-cache on install
